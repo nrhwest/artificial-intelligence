b399ec7710ba0d4476c99a57cbc554aecfa06e08
Derrick Adjei <adjeidr@vcu.edu>
2018-09-18 21:44:10 -0400

    new seperation line w/ accuracy and error

--- project1/project1.py
+++ project1/project1.py
@@ -7,6 +7,7 @@ CMSC 409
 
 import matplotlib.pyplot as pyplot
 import numpy as npy
+import sys
 
 # generate male and female weights and heights (data)
 male_weights = npy.random.normal(195.7, 20, 2000)
@@ -15,13 +16,73 @@ male_heights = npy.random.normal(5.9, .1, 2000)
 female_weights = npy.random.normal(168.5, 20, 2000)
 female_heights = npy.random.normal(5.4, .1, 2000)
 
+f_female_above= int() #TP
+f_female_below = int() #FP
+s_female_above= int() #TP
+s_female_below = int() #FP
+
+f_male_above = int()  #TN
+f_male_below = int()  #FN
+s_male_above = int()  #TN
+s_male_below = int()  #FN
+
+def above_below(weight, height, key, g = "male"):
+    above = int()
+    below = int()
+    if key == 'h':
+        if height > 5.65:
+            return True
+        else:
+            return False
+    if key == 'w':
+        if weight > (-142.65)*(height) + 990:
+            return True
+        else:
+            return False
+
+def acc_err(m_above, m_below, f_above, f_below):
+    ACC = (m_above+f_below) / (m_above + m_below + f_above + f_below)
+    ERR = 1 - ACC
+    return ACC, ERR
+
 # write data to file
 file = open("data.txt", "w")
 for i in range (0, 2000):
     file.write('%3.2f, %2.1f, 0\n' % (male_weights[i], male_heights[i]))
-
+    if above_below(male_weights[i], male_heights[i], key = 'h') == True:
+        abv = 1
+        blo = 0
+    else:
+        abv = 0
+        blo = 1
+    f_male_above += abv
+    f_male_below += blo
+    if above_below(male_weights[i], male_heights[i], key = 'w') == True:
+        abv = 1
+        blo = 0
+    else:
+        abv = 0
+        blo = 1
+    s_male_above += abv
+    s_male_below += blo
 for i in range(0, 2000):
     file.write('%3.2f, %2.1f, 1\n' % (female_weights[i], female_heights[i]))
+    if above_below(female_weights[i], female_heights[i], key = 'h') == True:
+        abv = 1
+        blo = 0
+    else:
+        abv = 0
+        blo = 1
+    f_female_above += abv
+    f_female_below += blo
+    if above_below(female_weights[i], female_heights[i], key = 'w', g= "female") == True:
+        abv = 1
+        blo = 0
+    else:
+        abv = 0
+        blo = 1
+    s_female_above += abv
+    s_female_below += blo
 
 file.close()
 
@@ -31,31 +92,39 @@ x = [i for i in range(len(female_heights))]
 pyplot.title("Male vs Female Heights")
 pyplot.xlabel("Student IDs")
 pyplot.ylabel("Male and Female Heights")
-pyplot.scatter(x, female_heights, color='r', marker='*')
-pyplot.scatter(x, male_heights, color='b', marker='x')
+pyplot.scatter(x, female_heights, color='r')
+pyplot.scatter(x, male_heights, color='b')
 # calculate separation line
 y = [2000 * 0 + 5.65 for i in x]
 pyplot.plot(x, y, color='g')
 pyplot.show()
 
 # plot male and female weights and heights
-import matplotlib.pyplot as pyplot2
-
-# calculate separation line
 yy = list()
 xx = list()
 for i in range(52,64):
     xx.append(i/10)
-for i in range(12):
-    print(i)
-    f = (-28.65*(i+2)) + 375
+for i in xx:
+    f = (-142.65)*(i) + 990
     yy.append(f)
-# yy.reverse()
 
-pyplot2.title("Both Weights and Heights Considered")
-pyplot2.xlabel("Male and Female Heights")
-pyplot2.ylabel("Male and Female Weights")
-pyplot2.scatter(male_heights, male_weights, color='b', marker='x')
-pyplot2.scatter(female_heights, female_weights, color ='r', marker='*')
-pyplot2.plot(xx, yy, color='g')
-pyplot2.show()
+pyplot.title("Both Weights and Heights Considered")
+pyplot.xlabel("Male and Female Heights")
+pyplot.ylabel("Male and Female Weights")
+# calculate separation line
+pyplot.scatter(male_heights, male_weights, color='b', marker='x')
+pyplot.scatter(female_heights, female_weights, color ='r', marker='*')
+pyplot.plot(xx, yy, color='g')
+pyplot.show()
+
+
+f_acc, f_err = acc_err(f_male_above, f_male_below, f_female_above, f_female_below)
+# print(f_male_above, f_male_below, f_female_above, f_female_below)
+print("----Scenario A----")
+print("Acc: {0:.3f}".format(f_acc))
+print("Err: {0:.3f}".format(f_err))
+print("----Scenario B----")
+s_acc, s_err = acc_err(s_male_above, s_male_below, s_female_above, s_female_below)
+# print(s_male_above, s_male_below, s_female_above, s_female_below)
+print("Acc: {0:.3f}".format(s_acc))
+print("Err: {0:.3f}".format(s_err))
