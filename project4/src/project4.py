f9203013ea94e657b2f7b291d1713c35734d0902
nrhwest <nrhwest@gmail.com>
2018-11-25 16:43:52 -0500

    sentence list ready from stemming

--- project4/src/project4.py
+++ project4/src/project4.py
@@ -8,33 +8,35 @@ import sys
 import string
 import Porter_Stemmer_Python as PorterStemmer
 
+
 def load_sentences():
     return re.sub(r"[^A-z \n]", "", open("sentences.txt", 'r').read().lower()).split('\n')
+
 
 def load_stop_words():
     return [word for line in open("stop_words.txt", 'r') for word in line.split()]
 
+
 def tokenize(sentence_list):
-    return list(map(lambda sentence: sentence.lower().split(), sentences))
+    return list(map(lambda sentence: sentence.lower().split(), sentence_list))
+
 
 def remove_stop_words(sentence_list, stop_words):
+    new_list = list()
     for line in sentence_list:
-        for word in line:
-            if word in stop_words:
-                print("before removal, line = ", line)
-                print("WORD = ", word)
-                line.remove(word)
-                print("after removal, line = ", line)
-                print()
-                # break
-        # break
-    return sentence_list
+        new_list.append(list(filter(lambda word: not stop_words.__contains__(word), line)))
+    return new_list
 
-sentences = load_sentences()
-stop_words = load_stop_words()
-# print(stop_words)
 
-sentence_list = tokenize(sentences)
-sentence_list = remove_stop_words(sentence_list, stop_words)
+def main():
 
-print(sentence_list)
+   sentences = load_sentences()
+   stop_words = load_stop_words()
+
+   sentence_list = tokenize(sentences)
+   sentence_list = remove_stop_words(sentence_list, stop_words)
+
+   print(sentence_list)
+
+if __name__ == '__main__':
+    main()
