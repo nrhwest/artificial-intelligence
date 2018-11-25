4d0477ed934d83f491698796ea19ad96e5da4866
Nathan West <natewest@Nathans-MacBook-Pro.local>
2018-11-25 16:20:33 -0500

    load sentences/stop words, tokenize and removal

--- project4/src/project4.py
+++ project4/src/project4.py
@@ -8,33 +8,33 @@ import sys
 import string
 import Porter_Stemmer_Python as PorterStemmer
 
-sentences = open("sentences.txt", mode='r')
-stop_words = open("stop_words.txt", mode='r')
+def load_sentences():
+    return re.sub(r"[^A-z \n]", "", open("sentences.txt", 'r').read().lower()).split('\n')
 
-# make a 2D list of sentences
-sentences_list = list(map(lambda sentence: sentence.lower().split(), sentences))
-# print(sentences_list)'
+def load_stop_words():
+    return [word for line in open("stop_words.txt", 'r') for word in line.split()]
 
-for lines in sentences_list:
-    for word in lines:
-        # if word.find()
-        if re.match(r'[^A-Za-z0-9]+', word):
-            re.sub(r'[^A-Za-z0-9]+', '', word)
-        if re.match(r'\d+', word):
-            ind = lines.index(word)
-            del lines[ind]
+def tokenize(sentence_list):
+    return list(map(lambda sentence: sentence.lower().split(), sentences))
 
-# porter = PorterStemmer()
+def remove_stop_words(sentence_list, stop_words):
+    for line in sentence_list:
+        for word in line:
+            if word in stop_words:
+                print("before removal, line = ", line)
+                print("WORD = ", word)
+                line.remove(word)
+                print("after removal, line = ", line)
+                print()
+                # break
+        # break
+    return sentence_list
 
-print(sentences_list)
-# the_list = list(map(lambda word: word.translate(string.punctuation), sentences_list))
-# all_tokens = []
-# for line in sentences:
-#     uniques = set(line.split())
-#     freqs = [(item, line.split(" ")) for item in uniques]
-#     print(freqs)
-#     print("\n\n")
-    # print(line, sep=' ', en   d='n', file=sys.stdout, flush=False)
-    # print(string.punctuation)
+sentences = load_sentences()
+stop_words = load_stop_words()
+# print(stop_words)
 
-# print(all_tokens)
+sentence_list = tokenize(sentences)
+sentence_list = remove_stop_words(sentence_list, stop_words)
+
+print(sentence_list)
