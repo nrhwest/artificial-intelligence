0c131df88614bcf392dd69b43b82bc605c4471d3
nrhwest <nrhwest@gmail.com>
2018-11-27 04:36:42 -0500

    complete project4

--- project4/src/project4.py
+++ project4/src/project4.py
@@ -1,3 +1,4 @@
+
 '''
 Written by Yonathan Mekonnen, Derrick Adeji, Nathan West
 CMSC 409
@@ -20,6 +21,7 @@ def tokenize(sentence_list):                      # return a list of tokens with
 
 def remove_stop_words(sentence_list, stop_words):
     new_sentence_list = list()
+
     for line in sentence_list:                    # remove/filter out stop_words from the sentence add to new list
         new_sentence_list.append(list(filter(lambda word: not stop_words.__contains__(word), line)))
 
@@ -29,13 +31,19 @@ def remove_stop_words(sentence_list, stop_words):
 def porter_stemming(sentence_list):
     stemmed_list = list()
     stemmer = PorterStemmer()                     # instantiate porter stemmer algorithm
+
     for line in sentence_list:                    # loop through sentence_list, stem each word, add to new list
         stemmed_list.append(list(map(lambda word: stemmer.stem(word, 0, len(word)-1), line)))
+
+    file = open("feature_vectors.csv", "w")
+    writer = csv.writer(file)
+    writer.writerows(stemmed_list)
 
     return stemmed_list
 
 def combined_stemmed_words(sentence_list):
     count = dict()
+
     for sentence in sentence_list:
         for word in sentence:
             if word in count:
@@ -48,54 +56,59 @@ def combined_stemmed_words(sentence_list):
 
 def create_tdm(occurrences, stemmed_list):
     tdm = list()
+
     for sentence in stemmed_list:                 # each sentence (list) in stemmed_list
         vector = [0] * len(occurrences)           # make freq_vector the size of length of occurrences
+
         for word in sentence:                     # each token in list
             if (word in occurrences):
                 tmp = list(occurrences)           # create a list of the stemmed words dictionary
                 vector[tmp.index(word)] += 1      # increment the words occurrence from a given sentence?
+
         tdm.append(vector)
+        tdm_file = open("tdm.csv", 'w')
+        writer = csv.writer(tdm_file)
+        writer.writerows(tdm)
 
     return tdm
 
 
-def print_to_csv(tdm):  # not sure if this is correct
-    with open("term_document_matrix.csv", 'w') as file:
-        writer = csv.writer(file, delimiter=',')
-        writer.writerow(tdm)
+def euclidean_distance(clusters, distances, weights, vector):
+    for i in range(clusters):
+        distances[i] = 0
+        for j in range(len(vector)):
+            val = (weights[i][j] - vector[j]) ** 2
+            distances[i] += val
 
 
-def wta_clustering(tdm, stemmed_list, training_size, alpha):
-    count = 5
-    weights = np.random.rand(5, len(tdm[0]))
+def wta_clustering(tdm, stemmed_list, training_size, alpha, clusters):
+    weights = np.random.rand(clusters, len(tdm[0]))
 
-    # print(weights)
-    # print(weights[0])
+    final = list()
+    distances = np.zeros(len(weights))
+    tdm = np.array(tdm)
+
     for i in range(training_size):
-
         for vector in tdm:
             distances = np.zeros(len(weights))
+            euclidean_distance(clusters, distances, weights, vector)
+            index = np.argmin(distances)                          # index of the best matching unit
+            weights[index] = weights[index] + alpha * vector    # update weight of the best matching unit/ winning cluster
 
-            for x in range(len(weights)):
-                distances[x] = 0
+    # run distances to retrieve final answers
+    for vector in tdm:
+        distances = np.zeros(len(weights))
+        euclidean_distance(clusters, distances, weights, vector)
+        index = np.argmin(distances)                            # index of the best matching unit
+        final.append(index)
 
-                for y in range(len(vector)):
-                    val = pow((vector[y] - weights[x][y]), 2)
-                    distances[x] += val
-
-            index = -1
-            for x in range(len(distances)):
-                if distances[x] < distances[index]:
-                    index = i
-            print(index)
-# def normalization(tdm):
-
+    return final
 
 
 def main():
-
-    training_size = 500
+    training_size = 1000
     alpha = 0.3
+    num_clusters = 8
 
     sentences = re.sub(r"[^A-z \n]", "", open("sentences.txt", 'r').read().lower()).split('\n')
     stop_words = open("stop_words.txt", 'r').read().split('\n')
@@ -107,10 +120,19 @@ def main():
     occurrences = combined_stemmed_words(stemmed_list)
 
     tdm = list(occurrences) + create_tdm(occurrences, stemmed_list)
-    # print_to_csv(tdm)
-
     tdm2 = create_tdm(occurrences, stemmed_list)
-    wta_clustering(tdm2, stemmed_list, training_size, alpha)
+
+    results = wta_clustering(tdm2, stemmed_list, training_size, alpha, clusters=num_clusters)
+
+    original_sentences = open("sentences.txt", 'r').read().split('\n')
+
+    for i in range(num_clusters):
+        print("Cluster {}: ".format(i))
+        print("------------")
+        for x in range(len(results)):
+            if results[x] == i:
+                print(original_sentences[x])
+
 
 if __name__ == '__main__':
     main()
